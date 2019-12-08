7f9ea8b1d4d833613257b5c3ef4cbeb64d0f3161
Peter Rydell <peterydell@gmail.com>
2019-12-07 20:14:05 -0800

    asynchronous function for adding additional members after manager added

--- index.js
+++ index.js
@@ -113,3 +113,71 @@ inquirer
     //adding team member to main template file
     AddorStop(proceed,user.member[0],team,templateMainFile);
 });
+
+//Render html with user inputs
+function renderHTML (position){
+    const templateFile = fs.readFileSync(`./templates/${position.getRole().toLowerCase()}.html`,  { encoding: 'utf8' });
+    let temporaryFile = templateFile.replace('{{ name }}', position.name);
+    temporaryFile = temporaryFile.replace('{{ role }}', position.getRole());
+    temporaryFile = temporaryFile.replace('{{ id }}', position.id);
+    temporaryFile = temporaryFile.replace('{{ email }}', position.email);
+    temporaryFile = temporaryFile.replace('{{ email }}', position.email);
+
+
+    if(position.getRole().toLowerCase()==="engineer"){
+        temporaryFile = temporaryFile.replace('{{ github }}', position.github);
+        temporaryFile = temporaryFile.replace('{{ github }}', position.github);
+      }else if(position.getRole().toLowerCase()==="intern"){
+        temporaryFile = temporaryFile.replace('{{ school }}', position.school);
+      }else if(position.getRole().toLowerCase()==="manager"){
+        temporaryFile = temporaryFile.replace('{{ officeNumber }}', position.officeNumber);
+    } return temporaryFile;
+}
+
+async function AddorStop (proceed,chosenMember,team, templateMainFile) {
+  try {
+    do{
+      switch(chosenMember){
+        case "engineer":
+        const engineer = await inquirer.prompt(engineerQs);
+        console.log(engineer);
+        
+        let engineerNew = new Engineer(engineer.name , engineer.id, engineer.email,engineer.github);
+        let engineerCard = renderHTML(engineerNew);
+        team = team + engineerCard
+        console.log(team);
+        let nextMove = await inquirer.prompt(addMore);
+        console.log(nextMove);
+        
+        if(nextMove.choice[0]==="false"){
+            proceed = false;
+            console.log(proceed,"THIS IS PROCEED STATUS");
+            let mainTemplate = templateMainFile.replace('{{ team }}', team);
+            fs.writeFileSync("index.html",mainTemplate);
+        } else if (nextMove.choice[0]==="true"){
+          const newMember = await inquirer.prompt(typeOfMember);
+          chosenMember = newMember.member1[0];
+        }
+        break;  
+        case "intern":
+        const intern = await inquirer.prompt(internQs);
+          let internNew = new Intern(intern.name , intern.id, intern.email,intern.school);
+          let internCard = renderHTML(internNew);
+          team = team + internCard
+          console.log(team);
+        let nextMove1 = await inquirer.prompt(addMore);
+         
+            if(nextMove1.choice[0]==="false"){
+              proceed = false;
+              let tempMain = templateMainFile.replace('{{ team }}', team);
+              fs.writeFileSync("index.html",tempMain);
+            } else if (nextMove1.choice[0]==="true"){
+              const newMember1 = await inquirer.prompt(memberType);
+              chosenMember = newMember1.member1[0];
+            }
+        break;
+      }
+    }while(proceed)
+  } catch (err) {
+  }
+}
